b9aef095eef43c1787d644c8bb47bb4e4016931d
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2022-10-13 23:35:19 +0200

    poco a poco

--- public/script.js
+++ public/script.js
@@ -1,20 +1,8 @@
-import styled from "daisyui/dist/styled";
-import { css } from "style.css";
+console.log("connected to script.js");
 
-console.log ("Connected to script.js");
+const header = document.querySelector ("header");
 
+window = addEventListener ("scroll", () => {
+  header.classList.toggle ("sticky", window.scrollY > 0);
+});
 
-
-const nav = document.querySelector(".nav");
-window.onscroll = () => {
-  this.scrolly = window.pageYOffset;
-  if (this.scrolly > 20) {
-    nav.classList.add("tw-ring-color");
-    nav.classList.remove("nav-transparent");
-  }
-  else {
-    nav.classList.remove("nav-colored");
-    nav.classList.add("nav-transparent");
-  }
-
-} 
